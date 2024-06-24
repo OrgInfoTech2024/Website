a694d79f5d906a660c7b64b55a80dd511587bbc9
OrgInfoTech <orginfotech@ukr.net>
2024-06-24 19:16:16 +0300

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,11 +1,11 @@
 function login() {
-    const email = document.getElementById("emailDashboard").value;
-    const password = document.getElementById("passwordDashboard").value;
+    const email = document.getElementById("emailDashboard").value.trim();
+    const password = document.getElementById("passwordDashboard").value.trim();
 
     if (workers[email] && workers[email].password === password) {
         sessionStorage.setItem("user", JSON.stringify(workers[email]));
         window.location.href = "dashboard.html";
     } else {
-        alert("Invalid email or password. Please try again.");
+        document.getElementById("error-message").textContent = "Invalid email or password. Please try again.";
     }
 }
